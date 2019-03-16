4d0ac2e299c281c6e3460680370e8aae3dfad450
Clinton Daniel <cedanie2@usf.edu>
2019-03-16 15:12:39 -0400

    Add project files.

---
+++ API_Simple/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API_Simple.Models;
+// ADD THESE DIRECTIVES
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace API_Simple.Controllers
+{
+    public class HomeController : Controller
+    {
+        string BASE_URL = "https://api.iextrading.com/1.0/";
+        HttpClient httpClient;
+
+        public HomeController()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new
+                System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /*
+            Calls the IEX reference API to get the list of symbols.
+            Returns a list of the companies whose information is available. 
+        */
+        public List<Company> GetSymbols()
+        {
+            string IEXTrading_API_PATH = BASE_URL + "ref-data/symbols";
+            string companyList = "";
+            List<Company> companies = null;
+
+            // Connect to the IEXTrading API and retrieve information
+            httpClient.BaseAddress = new Uri(IEXTrading_API_PATH);
+            HttpResponseMessage response = httpClient.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+
+            // Read the Json objects in the API response
+            if (response.IsSuccessStatusCode)
+            {
+                companyList = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+
+            // Parse the Json strings as C# objects
+            if (!companyList.Equals(""))
+            {
+                companies = JsonConvert.DeserializeObject<List<Company>>(companyList);
+                companies = companies.GetRange(0, 50);
+            }
+
+            return companies;
+        }
+
+        public IActionResult Index()
+        {
+            // Get the data from the List using GetSymbols method
+            List<Company> companies = GetSymbols();
+            // Send the data to the Index view
+            return View(companies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ API_Simple/Models/EF_Models.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Simple.Models
+{
+    public class Company
+    {
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string type { get; set; }
+        public string iexId { get; set; }
+        public bool isEnabled { get; set; }
+    }
+}
